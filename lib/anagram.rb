1c37b1d437b5e3a042a26535ca13353d24cc0556
Olivia Kesler <oakesler@protonmail.ch>
2019-08-14 04:34:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  
+attr_accessor :word, :words
+def initialize(word)
+  @word = word 
+end
+
+def match(words)
+  letter_array = []
+  @word = word
+  new_word  = word.split(',')
+  new_word.each do |letter|
+    letter_array << letter
+  end
+end
+end
+ 
+
