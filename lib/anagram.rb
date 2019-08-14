53f030e295d2fa8afcf641a2095e5f3cf50e950d
Olivia Kesler <oakesler@protonmail.ch>
2019-08-14 05:34:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  
+attr_accessor :word, :words
+def initialize(word)
+  @word = word 
+end
+
+def match(words)
+  final_array = []
+  words.each do |item|
+    if item.chars.sort == word.chars.sort
+      final_array << item
+    end
+  end
+  final_array
+end
+end
